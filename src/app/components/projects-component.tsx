4f7c65ea0e064b1603928b8346d252ee74f2374c
AlexiaCazale <alexiacazale7@gmail.com>
2025-09-30 23:05:45 -0300

    fix: corrigir erro de build

--- src/app/components/projects-component.tsx
+++ src/app/components/projects-component.tsx
@@ -1,37 +1,41 @@
-import PixelCard from "@/components/PixelCard";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+const PixelCard = dynamic(() => import('@/components/PixelCard'), {
+  ssr: false,
+});
+
 export const dataProjects = [
-    {
-      id: 0,
-      name: "Inteligência Artificial",
-      description:
-        "Projeto de apresentação realizado pela profª Cida Zem para os alunos da escola EMI Ana Franco, durante um bate-papo sobre inteligência artificial, tecnologia na educação, inclusão e a importância das mulheres na tecnologia.",
-      src: "/projects/ia-capa.jpeg",
-      alt: "Projeto 1",
-    },
-    {
-      id: 1,
-      name: "Girassol Arduino",
-      description:
-        "Projeto de Arduino desenvolvido pelos alunos do grupo de pesquisa Girls in STEM, apresentado durante o evento Fatec de Portas Abertas em 2024 e também para os estudantes da escola EMI Ana Franco, com a orientação dos professores Josiane e Felipe.",
-      src: "/projects/girassolCapa.jpeg",
-      alt: "Projeto 2",
-    },
-    {
-      id: 2,
-      name: "Girassol Arduino",
-      description:
-        "Projeto de Arduino desenvolvido pelos alunos do grupo de pesquisa Girls in STEM, apresentado durante o evento Fatec de Portas Abertas em 2024 e também para os estudantes da escola EMI Ana Franco, com a orientação dos professores Josiane e Felipe.",
-      src: "/projects/girassolCapa.jpeg",
-      alt: "Projeto 2",
-    },
-  ];
+  {
+    id: 0,
+    name: "Inteligência Artificial",
+    description:
+      "Projeto de apresentação realizado pela profª Cida Zem para os alunos da escola EMI Ana Franco, durante um bate-papo sobre inteligência artificial, tecnologia na educação, inclusão e a importância das mulheres na tecnologia.",
+    src: "/projects/ia-capa.jpeg",
+    alt: "Projeto 1",
+  },
+  {
+    id: 1,
+    name: "Girassol Arduino",
+    description:
+      "Projeto de Arduino desenvolvido pelos alunos do grupo de pesquisa Girls in STEM, apresentado durante o evento Fatec de Portas Abertas em 2024 e também para os estudantes da escola EMI Ana Franco, com a orientação dos professores Josiane e Felipe.",
+    src: "/projects/girassolCapa.jpeg",
+    alt: "Projeto 2",
+  },
+  {
+    id: 2,
+    name: "Girassol Arduino",
+    description:
+      "Projeto de Arduino desenvolvido pelos alunos do grupo de pesquisa Girls in STEM, apresentado durante o evento Fatec de Portas Abertas em 2024 e também para os estudantes da escola EMI Ana Franco, com a orientação dos professores Josiane e Felipe.",
+    src: "/projects/girassolCapa.jpeg",
+    alt: "Projeto 2",
+  },
+];
 
 export default function Projects() {
-  
+
   return (
     <React.Fragment>
       <div className="flex bg-[#2f3e43] justify-between h-[100%] w-[100%] lg:h-screen lg:w-screen p-[30px] md:px-16">
